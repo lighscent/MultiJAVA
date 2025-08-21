1bf7608f49f1a2ac51a999aba7108f46a82648a4
lighscent <augustin.pro.prs@gmail.com>
2025-08-21 02:10:45 +0200

    first commit

---
+++ java8.bat
@@ -0,0 +1,24 @@
+@echo off
+setlocal enabledelayedexpansion
+
+REM Définit Java 8
+set "JAVA_HOME=C:\Program Files\Java\jdk-1.8"
+
+REM Nettoie le PATH des anciens JAVA
+set "PATH_TMP="
+for %%i in ("!PATH:;=" "!") do (
+    set "item=%%~i"
+    echo !item! | findstr /i "Java" >nul
+    if errorlevel 1 (
+        if defined PATH_TMP (
+            set "PATH_TMP=!PATH_TMP!;!item!"
+        ) else (
+            set "PATH_TMP=!item!"
+        )
+    )
+)
+set "PATH=%JAVA_HOME%\bin;!PATH_TMP!"
+
+echo JAVA_HOME set to %JAVA_HOME%
+java %*
+endlocal
